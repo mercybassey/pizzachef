5663ab8f600ac96f7c7d239baa42395ff720fd8b
mercybassey <udohmercy911@gmail.com>
2020-10-24 11:22:30 -0700

    updated the application to be pwa compliant, added service worker, https as well as updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 import {store, persistor} from './redux/store';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,8 +20,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+
+serviceWorker.register();
  
