905beaeb2ef2897080a54e0036f0c57dcfdfc0de
mercybassey <udohmercy911@gmail.com>
2020-10-24 09:47:40 -0700

    added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
 
@@ -8,9 +9,22 @@ const StripeCheckoutButton = ({ price }) => {
    const publishableKey = 'pk_test_51HJI7xJPUg8KT8AHyj5WA9rZMFB24k9uWw6RudcQKdqVBw7z22l9lKpMIbcZHGOyEuvAjoarByNfLMblIkCKwlPn00LTpPzAb3';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+       axios({
+           url:'payment',
+           method: 'post',
+           data: {
+               amount: priceForStripe,
+               token
+           }
+       }).then(response => {
+           alert('Payment successful')
+       }).catch(error => {
+           console.log('Payment error: ', JSON.parse(error));
+           alert(
+               'There was an issue with your payment. Please make sure you use the provided credit card.'
+           );
+       });
+    };
 
 
    return (
@@ -19,13 +33,12 @@ const StripeCheckoutButton = ({ price }) => {
        name='Peppe Pizza'
        billingAddress
        shippingAddress
-       image='https://svgshare.com/i/Nz1.svg'
+       image='/img/peppes-pizza.svg'
        description={`Your total is ${price}`}
        amount={priceForStripe}
        panelLabel='Pay Now'
        token={onToken}
        stripeKey={publishableKey}
-    //    allowRememberMe
        />
        
    );
